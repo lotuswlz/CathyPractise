996f38ba1728b930371efa53b5eb5de2ce0aaa37
WuLingzi <lotuswlz@sina.com>
2015-05-23 17:36:50 +0800

    add html practise as index.html

---
+++ src/main/java/cathywu/practise/controller/MainController.java
@@ -0,0 +1,19 @@
+package cathywu.practise.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author lzwu
+ * @since 5/23/15
+ */
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String displayMainPage() {
+        return "index.html";
+    }
+}
