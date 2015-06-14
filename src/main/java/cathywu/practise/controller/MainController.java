9467fe5376ab69d1bfa3f9fe3a7427591413dc16
WuLingzi <lotuswlz@sina.com>
2015-06-15 00:22:47 +0800

    add index page and modified the access path

--- src/main/java/cathywu/practise/controller/MainController.java
+++ src/main/java/cathywu/practise/controller/MainController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @since 5/23/15
  */
 @Controller
-@RequestMapping("/main")
+@RequestMapping("/index")
 public class MainController {
 
     @RequestMapping(method = RequestMethod.GET)
